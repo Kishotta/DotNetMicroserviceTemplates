e725d510ef58fa97b7da19610dcea6109e2854f5
Connor Eaves <kishotta@gmail.com>
2022-11-24 01:02:10 -0600

    Move controller and swagger config to Presentation

--- src/Templates/Microservice/Microservice.Api/Program.cs
+++ src/Templates/Microservice/Microservice.Api/Program.cs
@@ -1,21 +1,15 @@
 using Microservice.Application;
 using Microservice.Infrastructure;
 using Microservice.Persistence;
+using Microservice.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices ();
 builder.Services.AddInfrastructureServices ();
 builder.Services.AddPersistenceServices (builder.Configuration);
-builder.Services.AddApplicationServices ();
-
-builder.Services
-       .AddControllers()
-       .AddApplicationPart(Microservice.Presentation.AssemblyReference.Assembly);
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddPresentationServices ();
 
 var app = builder.Build();
 
---
+++ src/Templates/Microservice/Microservice.Presentation/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservice.Presentation;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPresentationServices (this IServiceCollection services)
+    {
+        services.AddControllers();
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+}
